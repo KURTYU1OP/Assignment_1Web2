27607cd4d67825c041b8d305d41776a7855ab9a1
KURTYU1OP <adrianokurtiandencing@gmail.com>
2025-08-12 12:49:49 +0800

    Update db.php

--- Projects/db.php
+++ Projects/db.php
@@ -1,4 +1,15 @@
 <?php
 date_default_timezone_set('Asia/Manila');
-$conn = mysqli_connect("localhost", "root", "", "databasetest");
+
+$host = "b5jaiukhcikfck5vjkdl-mysql.services.clever-cloud.com";
+$user = "uacaq0sqq35fvnyh";
+$pass = "KYFsyO4WYDJPHSnZhOWK";
+$db   = "b5jaiukhcikfck5vjkdl";
+$port = 3306;
+
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
