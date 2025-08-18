345e39e948c8f68504ba921eca132fdd68fe0701
KURTYU1OP <adrianokurtiandencing@gmail.com>
2025-08-18 18:19:27 +0800

    Update index.php

--- Projects/index.php
+++ Projects/index.php
@@ -8,8 +8,11 @@ include_once "db.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="act3css.css">
     <title>Table</title>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+    
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -59,43 +62,56 @@ include_once "db.php";
     </style>
 </head>
 <body>
-    <!--  Table -->
-    <table>
-        <tr>
-            <th>Profile_ID</th>
-            <th>User_ID</th>
-            <th>Firstname</th>
-            <th>LastName</th>
-            <th>Posted-by</th>
-            <th>Posted-date</th>
-            <th>Modified-by</th>
-            <th>Modified-date</th>
-        </tr>
-        <?php
-        $query = "SELECT Profile_ID, User_ID, Firstname, LastName, PostedBy, 
-        PostedDate, ModifiedBy, ModifiedDate FROM `profile`";
-                
-        $result = $conn->query($query);
-
-        while($row = $result->fetch_assoc()) {
-            echo "
+    <!-- Table -->
+    <table id="myTable" class="display">
+        <thead>
             <tr>
-                <td>{$row['Profile_ID']}</td>
-                <td>{$row['User_ID']}</td>
-                <td>{$row['Firstname']}</td>
-                <td>{$row['LastName']}</td>
-                <td>{$row['PostedBy']}</td>
-                <td>{$row['PostedDate']}</td>
-                <td>{$row['ModifiedBy']}</td>
-                <td>{$row['ModifiedDate']}</td>
-            </tr>";
-        }
-        ?>
+                <th>Profile_ID</th>
+                <th>User_ID</th>
+                <th>Firstname</th>
+                <th>LastName</th>
+                <th>Posted-by</th>
+                <th>Posted-date</th>
+                <th>Modified-by</th>
+                <th>Modified-date</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $query = "SELECT Profile_ID, User_ID, Firstname, LastName, PostedBy, 
+                      PostedDate, ModifiedBy, ModifiedDate FROM `profile`";
+                    
+            $result = $conn->query($query);
+
+            while($row = $result->fetch_assoc()) {
+                echo "
+                <tr>
+                    <td>{$row['Profile_ID']}</td>
+                    <td>{$row['User_ID']}</td>
+                    <td>{$row['Firstname']}</td>
+                    <td>{$row['LastName']}</td>
+                    <td>{$row['PostedBy']}</td>
+                    <td>{$row['PostedDate']}</td>
+                    <td>{$row['ModifiedBy']}</td>
+                    <td>{$row['ModifiedDate']}</td>
+                </tr>";
+            }
+            ?>
+        </tbody>
     </table>
 
     <!-- Button -->
     <a href="1index.html">
         <button>Go to Personal Details</button>
     </a>
+
+    <script>
+        $(document).ready(function() {
+            $('#myTable').DataTable({
+                "pageLength": 5,   
+                "lengthMenu": [5, 10, 25, 50, 100]
+            });
+        });
+    </script>
 </body>
 </html>
